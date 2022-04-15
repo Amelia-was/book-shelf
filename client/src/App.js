8af1573a44e4cf9385249ad5473e045e90b02a23
Deisel <74831846+Amelia-was@users.noreply.github.com>
2022-04-15 10:47:20 -0400

    react router, pages setup

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './stylesheets/style.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>
+         stuff
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
